33059ec9827c66fc62d474aa67081dce8ba8c957
upworksanjeev <upworksanjeev@gmail.com>
2025-07-05 01:19:28 +0530

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,10 @@
 const serverless = require('serverless-http');
 const app = require('../../app');
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {
+  request: {
+    body: {
+      parser: "json"
+    }
+  }
+});

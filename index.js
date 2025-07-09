9d2190e761c0929ca513b435d00271de6b3af330
upworksanjeev <upworksanjeev@gmail.com>
2025-07-09 13:23:27 +0530

    attach intent to invoice

--- index.js
+++ index.js
@@ -414,6 +414,8 @@ app.post("/api/invoices/:id/pay", async (req, res) => {
 
     const invoice = await stripe.invoices.retrieve(invoiceId);
 
+
+
     if (!invoice) {
       return res.status(404).json({ error: "Invoice not found" });
     }
@@ -424,6 +426,14 @@ app.post("/api/invoices/:id/pay", async (req, res) => {
       customer: invoice.customer,
       payment_method_types: ["card_present"],
     });
+
+   await stripe.invoices.attachPayment(
+      invoiceId,
+      {
+        payment_intent: intent.id,
+        expand: ['payments'],
+      }
+    );
     res.json({ paymentIntentId: intent.id });
   } catch (error) {
     console.error("Error processing invoice payment:", error);
